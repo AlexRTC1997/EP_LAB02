d66f25db5908167b6ddb959e945187fbbf8526fd
AlexRTC1997 <aturpoco@unsa.edu.pe>
2023-09-23 09:13:03 -0500

    feature: Camera functionality

--- src/pages/CameraPage.tsx
+++ src/pages/CameraPage.tsx
@@ -1,8 +1,28 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './CameraPage.css';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import "./CameraPage.css";
 
-const Camera: React.FC = () => {
+import { useState } from "react";
+import { Camera, CameraResultType } from "@capacitor/camera";
+
+const CameraPage: React.FC = () => {
+  const [photo, setPhoto] = useState<string | undefined>();
+
+  const takePhoto = async () => {
+    const photoData = await Camera.getPhoto({
+      quality: 90,
+      resultType: CameraResultType.DataUrl,
+    });
+
+    setPhoto(photoData.dataUrl);
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -10,16 +30,13 @@ const Camera: React.FC = () => {
           <IonTitle>Camera</IonTitle>
         </IonToolbar>
       </IonHeader>
+
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Camera</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Camera page" />
+        <IonButton onClick={takePhoto}>Take a Photo</IonButton>
+        {photo && <img src={photo} alt="Captured" style={{ width: "100%" }} />}
       </IonContent>
     </IonPage>
   );
 };
 
-export default Camera;
+export default CameraPage;
