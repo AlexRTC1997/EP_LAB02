ca97197eedbe9bd8f28725fcfc241a2b28e6c9c1
AlexRTC1997 <aturpoco@unsa.edu.pe>
2023-09-23 18:03:06 -0500

    feature: Location functionality

--- src/pages/GpsPage.tsx
+++ src/pages/GpsPage.tsx
@@ -1,22 +1,57 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './GpsPage.css';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonLabel,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { Geolocation } from "@capacitor/geolocation";
+import { location as position } from "ionicons/icons";
+
+import "./GpsPage.css";
+import { useState } from "react";
 
 const GPS: React.FC = () => {
+  const [location, setLocation] = useState("Getting your geolocation...");
+
+  const getLocation = async () => {
+    const coordinates = await Geolocation.getCurrentPosition();
+
+    const {latitude, longitude} = coordinates.coords;
+
+    setLocation(`Latitude: ${latitude} \n Longitude: ${longitude}`);
+  };
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>GPS</IonTitle>
+          <IonTitle>GPS App</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
+      <IonContent className="ion-padding">
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">GPS</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="GPS page" />
+
+        <div className="photo-container">
+          {location && <IonLabel>{location}</IonLabel>}
+        </div>
+
+        <IonButton
+          onClick={getLocation}
+          expand="full"
+          fill="outline"
+          shape="round"
+        >
+          <IonIcon icon={position} className="ion-padding" />
+          Get your Location
+        </IonButton>
       </IonContent>
     </IonPage>
   );
